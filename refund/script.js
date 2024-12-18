c3ca52a9d15324b6215c6be7a37bc6e143890400
Caique Pires <c.pires@ba.estudante.senai.br>
2024-12-18 19:39:21 -0300

    Capturando evento de submit do formulario.
    E criando objeto para capturar detalhes da despesa do usuario.

--- refund/script.js
+++ refund/script.js
@@ -1,4 +1,7 @@
-amount = document.getElementById("amount")
+const form = document.querySelector("form")
+const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("expense")
 
 //função para capturar o valor do input
 amount.oninput = () => {
@@ -12,6 +15,7 @@ amount.oninput = () => {
     amount.value = formatCurrenciBRL(value)
 }
 
+//função para formatar o valor inserido no input automaticamente
 function formatCurrenciBRL(value){
     //formata o valor no padrão BRL
     value = value.toLocaleString("pt-BR", {
@@ -23,3 +27,20 @@ function formatCurrenciBRL(value){
     return value
 }
 
+// capturando evento de submit do formulario
+form.onsubmit = (event)=>{
+    //previne o comportamento de fazer rload
+    event.preventDefault()
+
+    //objeto com os detalhes da despesa do usuário
+    const newExpense = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value, 
+        category_name: category.options[category.selecIndex].text,
+        amount: amount.value, 
+        created_at: new Date(),
+
+    }
+
+}
